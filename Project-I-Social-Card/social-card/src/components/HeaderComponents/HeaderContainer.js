fdde190b888f4a6bc3fbd4155da4cc02b8fbd31b
zoverlvx <zoverhulser@gmail.com>
2019-03-25 15:43:30 -0400

    Replaces div with header.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -5,9 +5,9 @@ import HeaderTitle from "./HeaderTitle.js";
 import HeaderContent from "./HeaderContent.js";
 
 export default const HeaderContainer = () => (
-    <div>
+    <header>
         <ImageThumbnail url="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"/>
         <HeaderTitle />
         <HeaderContent />
-    </div>
+    </header>
 )
