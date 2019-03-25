5369bb433f7e11b752214f94b43ea35f1e5f728c
zoverlvx <zoverhulser@gmail.com>
2019-03-25 17:57:46 -0400

    Adds content to title.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from "./HeaderContent.js";
 
 const HeaderTitle = ({title, handle, timestamp}) => (
     <div className="header-title">
-        <h1>{title}</h1>
-        <span>{handle}</span><span>{timestamp}</span>
+        <div className="heading">
+            <h1>{title}</h1>
+            <span>{handle}</span><span>{timestamp}</span>
+        </div>
+        <HeaderContent 
+            text="Let's learn React by building simple interfaces with components"
+        />
     </div>
 )
 
