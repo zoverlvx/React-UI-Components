b80bba3637d41f7a9f72a28424549f2331a4aff8
zoverlvx <zoverhulser@gmail.com>
2019-03-25 15:53:27 -0400

    Implements props of Image component and Title component.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -4,10 +4,34 @@ import ImageThumbnail from "./HeaderThumbnail.js";
 import HeaderTitle from "./HeaderTitle.js";
 import HeaderContent from "./HeaderContent.js";
 
-export default const HeaderContainer = () => (
-    <header>
-        <ImageThumbnail url="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"/>
-        <HeaderTitle />
-        <HeaderContent />
-    </header>
-)
+export default const HeaderContainer = () => {
+    const date = new Date()
+    const day = date.getDate();
+    const month = date.getMonth();
+    const months = [
+        "jan",
+        "feb",
+        "mar",
+        "april",
+        "may",
+        "june",
+        "july",
+        "aug",
+        "sept",
+        "oct",
+        "nov",
+        "dec"
+    ]
+    return (
+        <header>
+            <ImageThumbnail url="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"/>
+            <HeaderTitle 
+                title="Lambda School"
+                handle="@LambdaSchool"
+                timestamp={`${day}/${months[month]}`}
+            />
+            <HeaderContent />
+        </header>
+    )
+}
+    
