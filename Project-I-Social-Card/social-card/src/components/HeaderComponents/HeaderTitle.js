eff4c4cb6e90529d8d4d24330556fcd6df74aa73
zoverlvx <zoverhulser@gmail.com>
2019-03-25 15:43:52 -0400

    Creates title section for header.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+export default const HeaderTitle = ({title, handle, timestamp}) => (
+    <div>
+        <h1>{title}</h1>
+        <span>{handle}</span><span>{timestamp}</span>
+    </div>
+)
