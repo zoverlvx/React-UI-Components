e900e7885066051513b4fe5289ff7c7e9e5b68b0
zoverlvx <zoverhulser@gmail.com>
2019-03-25 15:32:54 -0400

    Begins formation for the content of HeaderContainer.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from "./HeaderThumbnail.js";
+import HeaderTitle from "./HeaderTitle.js";
+import HeaderContent from "./HeaderContent.js";
+
+export default const HeaderContainer = () => (
+    <div>
+        <ImageThumbnail url="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"/>
+        <HeaderTitle />
+        <HeaderContent />
+    </div>
+)
