fd8181b01ce0b20331ffd9e01574842e55d316bf
zoverlvx <zoverhulser@gmail.com>
2019-03-26 18:56:06 -0400

    Project II Calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = ({buttonStyle, onClick, text}) => {
+    return (
+        <button
+            className={`general-styles ${buttonStyle}`}
+            onClick={onClick}
+        >
+            <div className="clear-text">{text}</div>
+        </button>
+    )
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = ({
+    customStyle, 
+    buttonStyle,
+    onClick,
+    text
+}) => {
+    return (
+        <button
+            className={`${customStyle} ${buttonStyle}`}
+            onClick={onClick}
+        >
+            <span className="number-text">{text}</span>
+        </button>
+    )
+}
+
+export default NumberButton;
